8e87bcf1d0326633e4f46d285052b6da62acae06
Umesh Patil <umeshpatil080@outlook.com>
2021-01-23 22:59:41 +0530

    Pushing initial calc package implemetation

---
+++ golang/modules/nummanip/calc/math.go
@@ -0,0 +1,6 @@
+package calc
+
+// Add - Returns sum of two numbers
+func Add(a int, b int) int {
+	return a + b
+}
---
+++ golang/modules/nummanip/go.mod
@@ -0,0 +1,3 @@
+module github.com/umeshpatil080/examples/tree/master/golang/modules/nummanip
+
+go 1.14
